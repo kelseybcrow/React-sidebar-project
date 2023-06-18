f48656601145187edde52e05071af7a331f8c894
kelseybcrow <kelseybcrow@gmail.com>
2023-06-18 21:38:20 +0200

    progress on context and state values

--- setup/src/Modal.js
+++ setup/src/Modal.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
+import { useGlobalContext } from './context'
+
 const Modal = () => {
+  const { isModalOpen, closeModal } = useGlobalContext()
+
   return (
-    <div className={`modal-overlay`}>
+    <div
+      className={`${
+        isModalOpen ? 'modal-overlay show-modal' : 'modal-overlay'
+      }`}
+    >
       <h3>modal content</h3>
       <button className='close-modal-btn'>
         <FaTimes />
--- setup/src/context.js
+++ setup/src/context.js
@@ -1,9 +1,44 @@
 import React, { useState, useContext } from 'react'
+import { useGlobalContext } from './../../final/src/context'
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true)
+  }
+  const closeSidebar = () => {
+    setIsSidebarOpen(false)
+  }
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+  const closeModal = () => {
+    setIsModalpen(false)
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSidebarOpen,
+        openModal,
+        openSidebar,
+        closeModal,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+// custom hook
+export const useGlobalContext = () => {
+  return useContext(AppContext)
 }
 
 export { AppContext, AppProvider }
